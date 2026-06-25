5d5f0ab61a5d1cab36151b5fec9b689759e226ee
SinusNatter8123 <konstantin.schoenig2.0@outlook.com>
2026-06-26 00:07:17 +0200

    Add .nojekyll file and update package.json for deployment scripts and dependencies

--- aischool/next.config.mjs
+++ aischool/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/PhiloKIWebsite" : "",
+  assetPrefix: isProd ? "/PhiloKIWebsite" : "",
 };
 
 export default nextConfig;
