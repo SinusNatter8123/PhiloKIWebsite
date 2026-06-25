20a9a0947f99fc5479383893ccf91c1802ee8d74
SinusNatter8123 <konstantin.schoenig2.0@outlook.com>
2026-06-25 23:46:00 +0200

    Updates

--- _next/static/TPbwnguF2xgewEjXUnASJ/_buildManifest.js
+++ _next/static/TPbwnguF2xgewEjXUnASJ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/PhiloKIWebsite//_next/:path+",
+        "destination": "/PhiloKIWebsite/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
